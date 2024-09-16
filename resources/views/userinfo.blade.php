7d129e1a48f200d790794046bd5aa95d80029aa8
Konstantin <k.kokhanenko@yandex.ru>
2024-09-16 20:02:17 +0300

    Add files via upload

---
+++ resources/views/userinfo.blade.php
@@ -0,0 +1,5 @@
+<ul>Текущий пользователь:
+    <li>Имя: {{ $user->name }} </li>
+    <li>email: {{ $user->email }} </li>
+    <li>id: {{ $user->id }} </li>
+</ul>
---
+++ storage/framework/views/56d63889f7599134e0d5374478d41202.php
@@ -0,0 +1,6 @@
+<ul>Текущий пользователь:
+    <li>Имя: <?php echo e($user->name); ?> </li>
+    <li>email: <?php echo e($user->email); ?> </li>
+    <li>id: <?php echo e($user->id); ?> </li>
+</ul>
+<?php /**PATH D:\Образование\Нетология\laravel\homework3\resources\views/userinfo.blade.php ENDPATH**/ ?>
